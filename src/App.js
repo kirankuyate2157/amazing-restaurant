99d89bfa522aec145efcd15ec0127d3568db3223
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-02-22 17:16:11 +0530

    heading design added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./app.css";
 import Navbar from "./components/navbar/Navbar";
+import Header from "./containers/header/Header";
 
 const App = () => {
   return (
     <>
       <div>
         <Navbar />
+        <Header />
       </div>
     </>
   );
