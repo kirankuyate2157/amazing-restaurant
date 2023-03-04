b21e5551db073b07445034651093d0cf25505243
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-03-04 13:40:34 +0530

    awards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./app.css";
 import Navbar from "./components/navbar/Navbar";
 import AboutUs from "./containers/aboutUs/AboutUs";
+import Awards from "./containers/awards/Awards";
 import Chef from "./containers/chef/Chef";
 import Header from "./containers/header/Header";
 import Intro from "./containers/intro/Intro";
@@ -17,6 +18,7 @@ const App = () => {
         <Menu />
         <Chef />
         <Intro />
+        <Awards />
       </div>
     </>
   );
