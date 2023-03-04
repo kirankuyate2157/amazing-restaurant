7839e790544c487129872fa47ae02418863e3300
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-03-04 17:12:57 +0530

    laocation and photos added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Navbar from "./components/navbar/Navbar";
 import AboutUs from "./containers/aboutUs/AboutUs";
 import Awards from "./containers/awards/Awards";
 import Chef from "./containers/chef/Chef";
+import Gallery from "./containers/gallery/Gallery";
 import Header from "./containers/header/Header";
 import Intro from "./containers/intro/Intro";
+import Location from "./containers/location/Location";
 import Menu from "./containers/menu/Menu";
 
 const App = () => {
@@ -19,6 +21,8 @@ const App = () => {
         <Chef />
         <Intro />
         <Awards />
+        <Gallery />
+        <Location />
       </div>
     </>
   );
